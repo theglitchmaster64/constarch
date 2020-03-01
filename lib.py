a8792059e837ec44707f1844ccf1a4781753edb0
theglitchmaster64 <44145041+theglitchmaster64@users.noreply.github.com>
2020-03-02 04:47:37 +0530

    ...
    ...

--- lib.py
+++ lib.py
@@ -2,7 +2,6 @@ import cv2
 import numpy
 import PIL
 from matplotlib import pyplot
-import argparse
 
 
 class colImg():
@@ -10,6 +9,8 @@ class colImg():
     def __init__(self,inputFilename):
         self.img = cv2.imread(inputFilename)
 
+    def info(self):
+        print('shape:{0}\ndepth:{1}'.format(self.img.shape,self.img.dtype))
 
     def conStretch(self,low,high):
         for i in range(0,3):
@@ -25,32 +26,44 @@ class colImg():
         histlist = []
         for i in range(0,3):
             histlist.append(cv2.calcHist([self.img],[i],None,[256],[0,256]))
-            for i in range(0,3):
+            for i in range(0,len(histlist)):
                 pyplot.plot(histlist[i])
-                pyplot.savefig('./hist_channel{}'.format(i))
+                pyplot.savefig('./histogram_channel{}'.format(i))
+                pyplot.clf()
 
 
     def write(self,outputFilename):
         cv2.imwrite(outputFilename,self.img)
+
+    def con2gray(self):
+        pass
 
 
 class greyImg():
 
     def __init__(self,inputFilename):
-        self.img = cv2.imread(inputFilename)
+        temp = cv2.imread(inputFilename)
+        self.img = cv2.cvtColor(temp,cv2.COLOR_BGR2GRAY)
+
+    def info(self):
+        print('shape:{0}\ndepth:{1}'.format(self.img.shape,self.img.dtype))
 
     def conStretch(self,low,high):
-        local_min = numpy.percentile(self.img[:,:,0],low)
-        local_max = numpy.percentile(self.img[:,:,0],high)
+        local_min = numpy.percentile(self.img[:,:],low)
+        local_max = numpy.percentile(self.img[:,:],high)
         gmin=0
         gmax=255
         print('running for channel 0\n\t{0}th percentile:{1}\n\t{2}th percentile:{3}'.format(low,local_min,high,local_max))
-        self.img[:,:,0] = (self.img[:,:,0] - local_min)*((gmax-gmin)/(local_max-local_min)) + gmin
+        self.img[:,:] = (self.img[:,:] - local_min)*((gmax-gmin)/(local_max-local_min)) + gmin
 
     def genHists(self):
         histogram = cv2.calcHist([self.img],[0],None,[256],[0,256])
         pyplot.plot(histogram)
-        pyplot.savefig('./hist_channelU')
+        pyplot.savefig('./histogram_grayscale')
+        pyplot.clf()
 
     def write(self,outputFilename):
         cv2.imwrite(outputFilename,self.img)
+
+    def colorize(self):
+        self.img = cv2.cvtColor(self.img,cv2.COLOR_GRAY2BGR)
--- main.py
+++ main.py
@@ -5,24 +5,35 @@ import numpy
 import PIL
 from matplotlib import pyplot
 import argparse
-import lib
+from lib import *
 
 parser = argparse.ArgumentParser(description='image manipulation module 3')
 parser.add_argument('input',help='input file name')
 parser.add_argument('output',help='output file name')
+parser.add_argument('--color',dest='color',action='store_true')
+parser.add_argument('--histogram',dest='hist',action='store_true')
+parser.add_argument('n1',type=float)
+parser.add_argument('n2',type=float)
 args = parser.parse_args()
 
 inputFile = args.input
 outputFile = args.output
-
-#img = cv2.imread(inputFile)
-#greyimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#print('shape:\t{0}\nbit-depth:{1}'.format(greyimg.shape,greyimg.dtype))
-
-
+n1 = args.n1
+n2 = args.n2
 
 
 if __name__ == '__main__':
-	col = lib.greyImg(inputFile)
-	col.conStretch(0.1,99.9)
-	col.write(outputFile)
+	if args.color:
+		col = colImg(inputFile)
+		col.info()
+		if args.hist:
+			col.genHists()
+		col.conStretch(n1,n2)
+		col.write(outputFile)
+	else:
+		gry = greyImg(inputFile)
+		gry.info()
+		if args.hist:
+			gry.genHists()
+		gry.conStretch(n1,n2)
+		gry.write(outputFile)
